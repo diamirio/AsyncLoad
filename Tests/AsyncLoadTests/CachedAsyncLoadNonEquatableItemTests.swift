f22986ed0ede0c15774317e20a99e2ef82299367
Alexander Kauer <contact@kaulex.dev>
2025-09-29 16:50:15 +0200

    added commented test cases

--- Tests/AsyncLoadTests/CachedAsyncLoadNonEquatableItemTests.swift
+++ Tests/AsyncLoadTests/CachedAsyncLoadNonEquatableItemTests.swift
@@ -8,10 +8,10 @@ struct CachedAsyncLoadNonEquatableItemTests {
         .init(.loading(), .loading()),
         .init(.loaded(NonEquatableItem(name: "Hello")), .loaded(NonEquatableItem(name: "Different"))),
         .init(.loaded(NonEquatableItem(name: "Some")), .loaded(NonEquatableItem(name: "Other"))),
-//        .init(.loading(NonEquatableItem(name: "Some")), .loading(NonEquatableItem(name: "Other"))),
-//        .init(.error(nil, TestingError.some), .error(nil, TestingError.some)),
-//        .init(.error(nil, TestingError.some), .error(NonEquatableItem(name: "Hello"), TestingError.other)),
-//        .init(.error(NonEquatableItem(name: "Hello"), TestingError.some), .error(NonEquatableItem(name: "World"), TestingError.some)),
+        .init(.loading(NonEquatableItem(name: "Some")), .loading(NonEquatableItem(name: "Other"))),
+        .init(.error(nil, TestingError.some), .error(nil, TestingError.some)),
+        .init(.error(nil, TestingError.some), .error(NonEquatableItem(name: "Hello"), TestingError.other)),
+        .init(.error(NonEquatableItem(name: "Hello"), TestingError.some), .error(NonEquatableItem(name: "World"), TestingError.some)),
     ])
     func structuralEquality(param: CachedAsyncLoadParameter<NonEquatableItem>) async throws {
         #expect(param.load1 == param.load2)
